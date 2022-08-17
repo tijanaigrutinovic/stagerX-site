d6166fc2ac01adc44c8a36a7ced106c1e25745a7
tijanaigrutinovic <tijana.igrutinovic@gmail.com>
2022-08-17 12:35:11 +0200

    small changes for site

--- js/search-box.js
+++ js/search-box.js
@@ -1,9 +1,27 @@
 $(".default_option").click(function(){
-    $(".dropdown ul").addClass("active");
-  });
-  
-  $(".dropdown ul li").click(function(){
-    var text = $(this).text();
-    $(".default_option").text(text);
-    $(".dropdown ul").removeClass("active");
-  });
+  $(".dropdown ul").addClass("active");
+});
+
+$(".dropdown ul li").click(function(){
+  var text = $(this).text();
+  $(".default_option").text(text);
+  $(".dropdown ul").removeClass("active");
+});
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
